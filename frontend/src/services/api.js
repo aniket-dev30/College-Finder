b6e12037198c99f791566b1c26d38f3f46f83cdb
Aniket Jha <134406663+aniket-dev30@users.noreply.github.com>
2026-05-17 12:24:03 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+baseURL: "https://college-finder-b6wp.onrender.com/api"
 });
 
 // ✅ GET all colleges (with search + filter + pagination)
